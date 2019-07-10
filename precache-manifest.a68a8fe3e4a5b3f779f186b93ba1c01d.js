8e2aaeb25de77f30acbd886ec5a57f8ac87ff29b
Tomasz Subik <tsubik@gmail.com>
2019-07-10 12:09:32 +0200

    Updates

---
+++ precache-manifest.a68a8fe3e4a5b3f779f186b93ba1c01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b482700d05e475d33d8996968f1ab6fd",
+    "url": "/test-app-deploy/index.html"
+  },
+  {
+    "revision": "b042889ff90e1c203f7a",
+    "url": "/test-app-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-app-deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b042889ff90e1c203f7a",
+    "url": "/test-app-deploy/static/js/main.574e8d26.chunk.js"
+  },
+  {
+    "revision": "01155d5c436a9116cff7",
+    "url": "/test-app-deploy/static/js/runtime~main.b7b8a50d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
